94b7bae083da9fb57cee0675359e7263bf4e71d3
julien <julien.milants@gmail.com>
2024-06-15 17:58:29 +0200

    ADD: delete SCSS, and add tailwind pour remplacer

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+'use client'
 import React from 'react'
-import '@/src/scss/App.scss'
+import './globals.css'
 import Navbar from '@/src/Components/Navbar'
 import Footer from '@/src/Components/Footer'
 import Link from 'next/link'
+import {Button} from "@nextui-org/button";
+
 
 export default function page() {
+    function test() {
+        console.log("test")
+    }
   return (
     <>
       <Navbar/>
       <div className="Home">
-        <h1>Hello World</h1>
+        <h1 className="text-blue-600">Hello World</h1>
         <Link href="/auth/signin">Signin</Link>
         <Link href="/auth/register">Register</Link>
       </div>
+        <Button>CVlick me</Button>
       <Footer/>
     </>
   )
