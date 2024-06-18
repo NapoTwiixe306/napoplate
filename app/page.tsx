0e763e3f0cc0309da0ac27955369e8010c542adf
julien <julien.milants@gmail.com>
2024-06-18 22:12:20 +0200

    add: ajout de CardData et page pour créer les Card fix: fix de home pour rendre semantic

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,41 @@ import Badge from '@/src/Components/Badges/Badges';
 import Image from 'next/image';
 import bg from '../src/img/Dashboard.jpg';
 import {SecondTextWithImage, TextWithImage} from '@/src/Components/TextWithCard/TextWithImage';
+import CardService from '@/src/Components/Card/CardService';
 
 export default function Page() {
   return (
     <>
       <div className="min-h-screen bg-white dark:bg-gray-900">
-        <div className="bg-white dark:bg-gray-900">
+        <header className="bg-white dark:bg-gray-900">
           <Navbar />
-          <div className="p-5 ">
-            <div className="flex flex-col items-center justify-center gap-5 mt-16">
-              <Badge />
-              <h1 className='text-6xl font-semibold text-black dark:text-white'>Build your SaaS in seconds</h1>
-              <p className='text-black dark:text-white'>This template demonstrates most of NapoPlate&#39;s components. It&#39;s a great starting point to build your own SaaS.</p>
-              <div className='mt-14'>
-                <Image className='rounded-xl' src={bg} alt='' width={1375} />
-              </div>
+        </header>
+        <main className="p-5">
+          <section className="flex flex-col items-center justify-center gap-5 mt-16">
+            <Badge />
+            <h1 className='text-6xl font-semibold text-black dark:text-white'>Build your SaaS in seconds</h1>
+            <p className='text-black dark:text-white'>This template demonstrates most of NapoPlate&#39;s components. It&#39;s a great starting point to build your own SaaS.</p>
+            <div className='mt-14'>
+              <Image className='rounded-xl' src={bg} alt='Dashboard background' width={1375} />
             </div>
-            {/* First */}
-            <div className="flex flex-col items-center justify-center mt-96">
-              <TextWithImage/>
-            </div>
-            {/* Second */}
-            <div className="flex flex-col items-center justify-center mt-52">
-              <SecondTextWithImage/>
-            </div>
-          </div>
-        </div>
+          </section>
+
+          {/* First Section */}
+          <section className="flex flex-col items-center justify-center mt-96">
+            <TextWithImage/>
+          </section>
+
+          {/* Second Section */}
+          <section className="flex flex-col items-center justify-center mt-52">
+            <SecondTextWithImage/>
+          </section>
+
+          {/*Card Section*/}
+          <section className='flex items-center justify-center mt-10'>
+            <CardService/>
+          </section>
+        </main>
+  
       </div>
     </>
   );
