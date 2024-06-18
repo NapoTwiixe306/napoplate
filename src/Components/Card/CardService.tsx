6f8be17ea35579558a84ed30e49c06bec018112e
julien <julien.milants@gmail.com>
2024-06-18 22:23:20 +0200

    tqt

--- src/Components/Card/CardService.tsx
+++ src/Components/Card/CardService.tsx
@@ -8,12 +8,14 @@ export default function CardService() {
       <div className="max-w-4xl px-4 py-12">
         <h2 className="mb-6 text-5xl font-bold text-center text-black dark:text-white">Why choose our service?</h2>
         <p className="mb-12 text-center text-black dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
           {services.map((service, index) => (
-            <div key={index} className="flex flex-col items-center w-full max-w-xs p-6 text-center transition-all duration-300 bg-gray-100 border-2 border-transparent rounded-lg shadow-lg dark:bg-gray-800 md:max-w-sm lg:max-w-md hover:border-blue-500 ">
-              <div className="mb-4 text-4xl">{service.icon}</div>
-              <h3 className="mb-2 text-xl font-semibold text-black dark:text-white">{service.title}</h3>
-              <p className='text-black dark:text-white'>{service.description}</p>
+            <div key={index} className="flex flex-col items-center w-full gap-3 p-6 text-center transition-all duration-300 bg-gray-100 border-2 border-transparent rounded-lg shadow-lg dark:bg-gray-800 hover:border-blue-500">
+              <div className='flex items-center gap-6'>
+                <div className="mb-4 text-4xl">{service.icon}</div>
+                <h3 className="mb-2 text-xl font-semibold text-black dark:text-white">{service.title}</h3>
+              </div>
+              <p className='text-black  dark:text-white'>{service.description}</p>
             </div>
           ))}
         </div>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -18,6 +18,7 @@ const Navbar: React.FC = () => {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
+
   return (
     <nav className="bg-white border-b border-gray-300 dark:bg-gray-900 dark:border-gray-700">
       <div className="flex items-center justify-between w-full h-20 p-5 mx-auto">
@@ -25,32 +26,47 @@ const Navbar: React.FC = () => {
           <h1 className="text-2xl font-bold text-black dark:text-gray-100">JMCorp/Boilerplate</h1>
         </div>
         <div className="hidden space-x-6 md:flex">
-          <Link href="/pages/Docs">
-            <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Docs</span>
-          </Link>
-          <Link href="/pages/service">
-            <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Service</span>
-          </Link>
-          <Link href="/pages/Price">
-            <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Price</span>
-          </Link>
+          <ul className="flex space-x-6">
+            <li>
+              <Link href="/pages/Docs">
+                <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Docs</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pages/service">
+                <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Service</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pages/Price">
+                <span className="text-lg font-bold text-black cursor-pointer dark:text-gray-100">Price</span>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="items-center hidden space-x-4 md:flex">
-          <div className="hidden space-x-6 md:flex">
-            <Link href="/auth/signin">
-              <span className="p-2 px-5 text-lg font-bold text-black border-2 rounded-full cursor-pointer dark:text-gray-100">Sign In</span>
-            </Link>
-            <Link href="/auth/register">
-              <span className="p-2 px-5 text-lg font-bold text-white bg-black rounded-full cursor-pointer dark:text-black dark:bg-white">Sign Up</span>
-            </Link>
-          </div>
-          <button onClick={toggleDarkMode}>{darkMode ? <FaSun className="w-6 h-6 text-black dark:text-gray-100" /> : <FaCat className="w-6 h-6 text-black dark:text-gray-100" />}</button>
+          <ul className="flex space-x-6">
+            <li>
+              <Link href="/auth/signin">
+                <span className="p-2 px-5 text-lg font-bold text-black border-2 rounded-full cursor-pointer dark:text-gray-100">Sign In</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/auth/register">
+                <span className="p-2 px-5 text-lg font-bold text-white bg-black rounded-full cursor-pointer dark:text-black dark:bg-white">Sign Up</span>
+              </Link>
+            </li>
+          </ul>
+          <button onClick={toggleDarkMode}>
+            {darkMode ? <FaSun className="w-6 h-6 text-black dark:text-gray-100" /> : <FaCat className="w-6 h-6 text-black dark:text-gray-100" />}
+          </button>
         </div>
         <div className="md:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
             type="button"
             className="text-black dark:text-gray-100 focus:outline-none"
+            aria-label="Toggle menu"
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
@@ -60,27 +76,39 @@ const Navbar: React.FC = () => {
       </div>
       {isOpen && (
         <div className="bg-gray-100 dark:bg-gray-900 md:hidden">
-          <div className="flex flex-col items-start p-5 space-y-2">
-            <Link href="/pages/Docs">
-              <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Docs</span>
-            </Link>
-            <Link href="/pages/service">
-            <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Service</span>
-            </Link>
-            <Link href="/pages/Price">
-              <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Price</span>
-            </Link>
-          </div>
-          <div className="flex flex-col items-start p-5 space-y-2">
-            <Link href="/auth/signin">
-              <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Sign In</span>
-            </Link>
-            <Link href="/auth/register">
-              <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Sign Up</span>
-            </Link>
-          </div>
+          <ul className="flex flex-col items-start p-5 space-y-2">
+            <li>
+              <Link href="/pages/Docs">
+                <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Docs</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pages/service">
+                <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Service</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/pages/Price">
+                <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Price</span>
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex flex-col items-start p-5 space-y-2">
+            <li>
+              <Link href="/auth/signin">
+                <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Sign In</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/auth/register">
+                <span className="text-lg text-black cursor-pointer dark:text-gray-100" onClick={() => setIsOpen(false)}>Sign Up</span>
+              </Link>
+            </li>
+          </ul>
           <div className="flex items-center justify-start p-5 space-x-4">
-            <button onClick={toggleDarkMode}>{darkMode ? <FaSun className="w-6 h-6 text-black dark:text-gray-100" /> : <FaCat className="w-6 h-6 text-black dark:text-gray-100" />}</button>
+            <button onClick={toggleDarkMode}>
+              {darkMode ? <FaSun className="w-6 h-6 text-black dark:text-gray-100" /> : <FaCat className="w-6 h-6 text-black dark:text-gray-100" />}
+            </button>
           </div>
         </div>
       )}
